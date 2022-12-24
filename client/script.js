6887f53d48f9120270a0cf0c7c63d57e7cecae2b
Tareq Personal <tareqpersonal@Tareqs-MacBook-Pro.local>
2022-12-24 19:15:28 +0300

    third commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://openai-codex-7l8v.onrender.com/", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
